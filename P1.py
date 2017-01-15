2e7e3d1b5e9f44fd58b4cf0bd7039bb49e66355f
lorenmh <neutronstar@gmail.com>
2017-01-15 12:21:12 -0800

    more changes

--- P1.py
+++ P1.py
@@ -1,15 +1,27 @@
 #!/opt/python-3.4/linux/bin/python3
+import re
+import sys
+
 import numpy as np
 import scipy as sp
+
+from itertools import permutations as permute
+
 from scipy.sparse import csc_matrix as sparse_matrix
-import re, sys
 
 SPLIT_RE_STR = r',\s?'
 SPLIT_RE = re.compile(SPLIT_RE_STR)
 
+MAT_DTYPE = np.uint64
+
+
 class InputError(Exception):
+    '''
+foo
+    '''
     def __init__(self, message):
         self.message = message
+
 
 def parse_input_line(input_line):
     input_list = SPLIT_RE.split(input_line)
@@ -30,6 +42,7 @@ def parse_input_line(input_line):
         raise InputError('Invalid rating "%s"' % rating_str)
     return user_id, movie_id, rating
 
+
 def group_by_user_reducer(accumulator, user_movie_rating):
     user_id, movie_id, rating = user_movie_rating
     movie_ratings_for_user = accumulator.get(user_id, [])
@@ -37,27 +50,35 @@ def group_by_user_reducer(accumulator, user_movie_rating):
     accumulator[user_id] = movie_ratings_for_user
     return accumulator
 
+
+def generate_user_occurance_matrix(movies_seen_by_user, size):
+    user_occurance_matrix = sparse_matrix((size, size), dtype=MAT_DTYPE)
+    permutations = permute(movies_seen_by_user, 2)
+
+    for movie_1, movie_2 in permutations:
+        user_occurance_matrix[movie_1, movie_2] = 1
+        user_occurance_matrix[movie_2, movie_1] = 1
+    for movie in movies_seen_by_user:
+        user_occurance_matrix[movie, movie] = 1
+
+
 # Filter out all empty lines
 input_lines = filter(len, sys.stdin)
 
-# Parse the CSV values from the lines of input
-user_movie_ratings = map(parse_input_line, input_lines)
+'''Ratings is a list of 3-tuples, (user_id, movie_id, rating)
+'''
+ratings = map(parse_input_line, input_lines)
 
-# Calculate the maximum movie id to create a sparse matrix
-max_movie_id = max(user_movie_ratings, key=lambda umr: umr[1])[1]
-# Create the sparse matrix
-co_occurance_matrix = sparse_matrix(
-        (max_movie_id, max_movie_id),
-        dtype=np.uint64
+# To get the max movie id
+mat_size = max(ratings, key=lambda r: r[1])[1]
+
+# start with an empty co-occurance matrix
+co_occurance_matrix = sparse_matrix((mat_size, mat_size), dtype=MAT_DTYPE)
+
+movie_ratings_grouped_by_users = reduce(
+    group_by_user_reducer,
+    ratings,
+    {}
 )
 
-movie_ratings_for_users = reduce(
-        group_by_user_reducer,
-        user_movie_ratings,
-        {}
-)
-
-movie_ratings_for_users_sorted = {
-        k:sorted(v) for (k,v) in movie_ratings_for_users.iteritems()}
-
-print(movie_ratings_for_users_sorted)
+print(movie_ratings_grouped_by_users)
