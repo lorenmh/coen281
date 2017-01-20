16e3a2a7239958aef036d9b5e6e5a458380909a8
lorenmh <neutronstar@gmail.com>
2017-01-19 23:27:11 -0800

    co-occ done, working on user

--- P1.py
+++ P1.py
@@ -8,7 +8,7 @@ import numpy as np
 import scipy as sp
 
 from functools import reduce
-from itertools import permutations as permute
+from itertools import combinations
 
 from scipy.sparse import (csc_matrix, csr_matrix, coo_matrix, dok_matrix)
 
@@ -62,32 +62,82 @@ try:
     f = open('sample_input.csv', 'r')
     ratings_input = list(parsed_input_generator(f))
     f.close()
+
+    # ratings_input = list(parsed_input_generator(sys.stdin))
 except InputError as e:
     # there was an InputError so record the error and exit
     print('Error with input:', e.message, file=sys.stderr)
     sys.exit(1)
 
-user_ids =  [input[0] for input in ratings_input]
-movie_ids = [input[1] for input in ratings_input]
-ratings =   [input[2] for input in ratings_input]
+# user_ids are the rows
+sequence_user_ids   = [input[0] for input in ratings_input]
+# movie_ids are the cols
+sequence_movie_ids  = [input[1] for input in ratings_input]
+# ratings are the values
+sequence_ratings    = [input[2] for input in ratings_input]
 
-size_user_id = max(user_ids) + 1
-size_movie_id = max(movie_ids) + 1
+# used to construct the matrices
+unique_user_ids = np.unique(sequence_user_ids)
+unique_movie_ids = np.unique(sequence_movie_ids)
 
-# ratings_mat = csr_matrix((ratings, (movie_ids, user_ids)))
-ratings_mat = np.zeros(shape=(size_movie_id, size_user_id))
-ratings_mat[movie_ids, user_ids] = ratings
+size_user_id = max(unique_user_ids) + 1
+size_movie_id = max(unique_movie_ids) + 1
 
-co_occurance_mat = np.zeros(shape=(size_movie_id, size_movie_id))
+ratings_arr = np.zeros(shape=(size_user_id, size_movie_id))
+ratings_arr[sequence_user_ids, sequence_movie_ids] = sequence_ratings
 
-for user_ratings in ratings_mat.T:
+co_occurance_arr = np.zeros(shape=(size_movie_id, size_movie_id))
+
+for user_ratings in ratings_arr:
     movies_rated_by_user = np.flatnonzero(user_ratings)
-    co_occurance_pairs = permute(movies_rated_by_user, 2)
-    user_co_occurance_mat = dok_matrix((size_movie_id, size_movie_id))
+    co_occurance_pairs = combinations(movies_rated_by_user, 2)
     for movie_id_1, movie_id_2 in co_occurance_pairs:
-        user_co_occurance_mat[movie_id_1, movie_id_2] = 1
+        co_occurance_arr[movie_id_1, movie_id_2] += 1
+        co_occurance_arr[movie_id_2, movie_id_1] += 1
     for movie_id in movies_rated_by_user:
-        user_co_occurance_mat[movie_id, movie_id] = 1
-    co_occurance_mat += user_co_occurance_mat
+        co_occurance_arr[movie_id, movie_id] += 1
 
-recommendation_mat = co_occurance_mat * ratings_mat
+recommendation_arr = co_occurance_arr.dot(ratings_arr.T)
+
+unseen_recs_arr = np.where(ratings_arr == 0, recommendation_arr.T, 0)
+
+''' enumerates each row of user recommendations to get the user id
+Then the user recommendations are enumerated so that each recommendation value
+contains the movie_id as well (the index is the movie id). Then recommendations
+with zero values are filtered out. Then the recommendations are sorted by the
+recommendation value. Then we print
+'''
+print('='*80)
+print('CO-OCCURANCE RECOMMENDATIONS')
+print('='*80)
+for id, user_recommendations in enumerate(unseen_recs_arr):
+    if not user_recommendations.any():
+        continue
+    recs_with_movie_id = enumerate(user_recommendations)
+    filtered_recs = filter(lambda i: i[1] != 0, recs_with_movie_id)
+    sorted_recs = sorted(filtered_recs, key=lambda i: i[1], reverse=1)
+    if len(sorted_recs):
+        print('user', id, sorted_recs, '=> recommend', sorted_recs[0][0])
+    else:
+        print('user', id, '=> no recommendation')
+print('='*80, '\n', '='*80, sep='')
+
+def compute_similarity(u, v):
+    non_zero_indices = np.intersect1d(np.nonzero(u), np.nonzero(u))
+    if not len(non_zero_indices):
+        return -1
+    u = u[non_zero_indices]
+    v = v[non_zero_indices]
+    return u.dot(v) / (np.linalg.norm(u) * np.linalg.norm(v))
+
+user_similarity_arr = np.identity(size_user_id)
+
+user_id_pairs = combinations(unique_user_ids, 2)
+
+for user_id_1, user_id_2 in user_id_pairs:
+    similarity = compute_similarity(
+        ratings_arr[user_id_1],
+        ratings_arr[user_id_2]
+    )
+    user_similarity_arr[user_id_1, user_id_2] = similarity
+    user_similarity_arr[user_id_2, user_id_1] = similarity
