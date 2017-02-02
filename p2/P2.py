d71a6a3643c693416d27391410ef17860e326644
lorenmh <neutronstar@gmail.com>
2017-02-01 21:25:07 -0800

    almost done with Girvan-Newman Algorithm

--- p2/P2.py
+++ p2/P2.py
@@ -14,17 +14,12 @@ import scipy as sp
 from functools import reduce
 from itertools import combinations
 
-from scipy.sparse import (csc_matrix, csr_matrix, coo_matrix, dok_matrix)
-
 np.set_printoptions(precision=3)
 
 INPUT_RE_STR = r'^(?:\s*#.*)|^(?:\s*(?P<a>[A-Za-z]+)\s*,\s*(?P<b>[A-Za-z]+)' \
     '\s*(?:#.*)?)$'
 
 INPUT_RE = re.compile(INPUT_RE_STR)
-
-# scipy throws warnings which we do not want to see
-warnings.filterwarnings('ignore')
 
 
 class InputError(Exception):
@@ -35,34 +30,86 @@ class InputError(Exception):
 def parsed_input_generator(input_lines):
     for input_line in input_lines:
         match = INPUT_RE.match(input_line)
-
-        # there was no match at all, this line is not valid!
         if match is None:
             raise InputError('Invalid input `%s`' % input_line.rstrip('\n'))
-
         groups = match.groups()
-
-        # this line starts with a comment so we will skip it
+        # groups will only contain None if the input was a comment
         if None in groups:
             continue
-
-        # unpack the groups, which is a 3-tuple of matched strings
-        a_str, b_str = groups
-
-        # have the generator yield a 3-tuple of the parsed values
-        yield a_str, b_str
+        yield groups
 
 
 try:
-    # for every input line, parse the line and put the values into ratings
+    # parse the input
     f = open('sample_input.csv', 'r')
     graph_input = list(parsed_input_generator(f))
     f.close()
     #ratings_input = list(parsed_input_generator(sys.stdin))
 except InputError as e:
-    # there was an InputError so record the error and exit
     print('Error with input:', e.message, file=sys.stderr)
     sys.exit(1)
 
-input_lower = [list(map(lambda s: s.lower(), t)) for t in graph_input]
-input_sorted = [sorted(l) for l in input_lower]
+# graph is represented as a dictionary. The keys are node_ids, the values are
+# lists of node_ids which that node is connected to. For example,
+# graph['A'] == ['B','C'] would mean that node A is connected to nodes B and C
+graph = {}
+for a, b in graph_input:
+    a_edges = graph.get(a, [])
+    b_edges = graph.get(b, [])
+
+    a_edges.append(b)
+    b_edges.append(a)
+
+    graph[a] = a_edges
+    graph[b] = b_edges
+
+
+def gna(root_node_id, graph):
+    '''Girvan-Newman Algorithm for determining betweenness of edges.
+    I have combined a number of steps into one single BFS. In the algorithm
+    outlined in the book, there are 3 BFS performed.  In my BFS, I set the
+    parents for each node. In the same BFS I determine whether a node is a leaf
+    or not. I also group each level so when I begin calculating the betweenness
+    I can walk from the deepest leaves and move up'''
+
+    parent_nodes = {root_node_id: [None]}
+    leaf_nodes = []
+    levels = []
+
+    already_visited = set([])
+    current_level = set([root_node_id])
+    next_level = set([])
+
+    while True:
+        for parent_node_id in current_level:
+            connected_nodes = graph[parent_node_id]
+
+            if not len(set(connected_nodes) - already_visited - current_level):
+                leaf_nodes.append(parent_node_id)
+                continue
+
+            for child_node_id in connected_nodes:
+                if child_node_id in (already_visited | current_level):
+                    continue
+
+                parents = parent_nodes.get(child_node_id, [])
+                parents.append(parent_node_id)
+                parent_nodes[child_node_id] = parents
+
+                next_level.add(child_node_id)
+
+        already_visited |= current_level
+        levels.append(current_level)
+
+        if len(next_level):
+            current_level = next_level
+            next_level = set([])
+        else:
+            break
+
+    parent_lens = {k: len(v) for k, v in parent_nodes.items()}
+
+    return parent_nodes, parent_lens, leaf_nodes, levels
+
+
+node_ids = set([i for sublist in graph_input for i in sublist])
